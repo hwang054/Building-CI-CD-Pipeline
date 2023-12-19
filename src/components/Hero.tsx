1d28f91159917932347c976b8368f48fd8538b1b
hwang054 <154353815+hwang054@users.noreply.github.com>
2023-12-19 11:25:37 -0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -56,15 +56,13 @@ const Hero = () => {
     <div className="flex flex-col py-24 items-center mx-2.5">
       <div>
         <h1 className="font-Lexend text-4xl md:text-5xl text-center text-lightSlate leading-tight tracking-tight">
-          {`Find `}
+          {`This is`}
 
           <span className="bg-gradient-to-r from-gradFirst via-gradMiddle to-gradLast bg-clip-text text-transparent">
-            Open-Source Repositories
+            Hasan Wang's repository
           </span>
 
           <br />
-
-          to contribute today
         </h1>
       </div>
 
